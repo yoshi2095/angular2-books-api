c31cf8767fcd6dbae15175b3f860195b5b6d70f6
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-11-23 18:08:11 +0530

    Upgrading to angular 2

--- dist/services/api.service.js
+++ dist/services/api.service.js
@@ -10,6 +10,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var http_1 = require('@angular/http');
+require('rxjs/add/operator/map');
 var Api = (function () {
     function Api(http) {
         this.http = http;
--- systemjs.config.js
+++ systemjs.config.js
@@ -4,6 +4,10 @@
  */
 (function(global) {
 	System.config({
+		paths: {
+			// paths serve as alias
+			'npm:': 'node_modules/'
+		},
 		// map tells the System loader where to look for things
 		map: {
 			// our app is within the app folder
