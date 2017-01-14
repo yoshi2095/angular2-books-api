ece288fcd40b632037f2aad06c00c3b895e07976
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2017-01-14 19:29:46 +0530

    Refinements

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,10 @@ gulp.task('clean', function() {
 });
 
 gulp.task('copy:nodemodules', function() {
-	return gulp.src(['node_modules/@angular/**/*', 'node_modules/rxjs/**/*', 'node_modules/angular2-in-memory-web-api/**/*', '!node_modules/**/*.tlog', '!node_modules/**/*.png', '!node_modules/**/*.jpg', '!node_modules/**/*.md', '!node_modules/**/*.txt', '!node_modules/**/LICENSE', '!node_modules/**/license', '!node_modules/**/*.markdown', '!node_modules/**/example/**/*', '!node_modules/**/examples/**/*', '!node_modules/**/test/**/*', '!node_modules/**/benchmark/**/*', '!node_modules/**/doc/**/*', '!node_modules/**/docs/**/*', '!node_modules/**/testData/**/*', '!node_modules/**/build/Debug/**/*', '!node_modules/**/build/deps/**/*'], {
+	return gulp.src(['node_modules/@angular/**/*', 'node_modules/rxjs/**/*', 'node_modules/angular-in-memory-web-api/**/*', '!node_modules/**/*.tlog', '!node_modules/**/*.png', '!node_modules/**/*.jpg', '!node_modules/**/*.md', '!node_modules/**/*.txt', '!node_modules/**/LICENSE', '!node_modules/**/license', '!node_modules/**/*.markdown', '!node_modules/**/example/**/*', '!node_modules/**/examples/**/*', '!node_modules/**/test/**/*', '!node_modules/**/benchmark/**/*', '!node_modules/**/doc/**/*', '!node_modules/**/docs/**/*', '!node_modules/**/testData/**/*', '!node_modules/**/build/Debug/**/*', '!node_modules/**/build/deps/**/*'], {
 			base: 'node_modules',
 		})
-		.pipe(debug({
-			title: 'copying node modules'
-		}))
-		.pipe(newer('dist/node_modules'))
+		.pipe(newer('dist/node_modules/'))
 		.pipe(gulp.dest('dist/node_modules'));
 });
 
@@ -63,7 +60,7 @@ gulp.task('compile', function() {
 });
 
 // Run browsersync for development
-gulp.task('serve', ['build'], function() {
+gulp.task('serve', ['complete-build'], function() {
 	browserSync({
 		server: {
 			baseDir: 'dist'
@@ -73,9 +70,13 @@ gulp.task('serve', ['build'], function() {
 	gulp.watch(['app/**/*', 'index.html'], ['buildAndReload']);
 });
 
-gulp.task('build', function() {
+gulp.task('complete-build', function() {
 	runSequence('clean', 'compile', 'copy:libs', 'copy:assets', 'copy:nodemodules');
 });
 
-gulp.task('buildAndReload', ['build'], reload);
-gulp.task('default', ['build']);
+gulp.task('lite-build', function() {
+	runSequence('clean', 'compile', 'copy:libs', 'copy:assets');
+});
+
+gulp.task('buildAndReload', ['lite-build'], reload);
+gulp.task('default', ['complete-build']);
