4f263ba0181d3367662d8e3e5506f56b243335c1
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2017-01-15 23:56:00 +0530

    integrating gulp and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
-module.exports = require('./config/webpack.dev.js');
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    'vendor': './app/ts/vendor.ts',
+    'app': './app/ts/bootstrap.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+  ],
+
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+
+  module: {
+    loaders: [{
+      test: /\.ts$/,
+      loader: 'ts-loader'
+    }, ],
+    noParse: [/angular2\/bundles\/.+/]
+  },
+
+  devServer: {
+    historyApiFallback: true
+  }
+};
